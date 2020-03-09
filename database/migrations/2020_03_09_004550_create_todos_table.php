142e4fb2526ab592c68660412875643c1d0028c9
sixpeteunder <plenjo15@gmail.com>
2020-03-09 05:35:03 +0300

    Initial Commit
    Setup project
    Write migrations

---
+++ database/migrations/2020_03_09_004550_create_todos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject', 255);
+            $table->string('content', 255)->default('');
+            $table->boolean('context');
+            $table->timestamp('due_on')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
