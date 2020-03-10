d77c3df0cf92c1890f2e2ea0d78d201588bd249d
sixpeteunder <plenjo15@gmail.com>
2020-03-10 10:19:31 +0300

    Add tags

--- app/Todo.php
+++ app/Todo.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Tags\HasTags;
 
 class Todo extends Model
 {
     use SoftDeletes;
+    use HasTags;
 
     /**
      * The model's default values for attributes.
