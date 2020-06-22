291100ee81e99b96ad1107776c9c2c53d04f991c
sixpeteunder <plenjo15@gmail.com>
2020-06-22 14:08:14 +0300

    Minor fixes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::view('/', 'welcome')->name('welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::view('/todos/new', 'create', ["tags" => Tag::all()])->name('create')->middleware('auth');
+Route::view('/todos/new', 'create', ["tags" => Tag::all()])->name('create')
 Route::post('todos/new', 'HomeController@create')->middleware('auth');
 Route::get('/todos', 'HomeController@todos')->name('todos')->middleware('auth');
 Route::get('/todos/{todo}', 'HomeController@todo')->middleware('auth');
